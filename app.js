8331a2461ff9c0d05e40e8b6a91886aa06386da2
juernevte <1612674@student.hcmus.edu.vn>
2020-07-07 21:00:07 +0700

    clg

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(limiter);
 
-console.log("\n"+MONGODB_URI+"\n");
+console.log("\n"+process.env.MONGODB_URI+"\n");
 (async () => {
   try {
     const dbInfo = await db.connectDB(process.env.MONGODB_URI);
